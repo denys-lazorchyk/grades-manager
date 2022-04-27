7feb16bf76641ac1045b6bfde240f8284846bcae
Denys Lazorchyk <denyslazorchyk@gmail.com>
2022-04-27 23:08:16 +0200

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { ControllerComponent } from './controller/controller.component';
+import { GradesComponent } from './grades/grades.component';
+import { GradeComponent } from './grades/grade/grade.component';
+import { EditGradeComponent } from './grades/edit-grade/edit-grade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    ControllerComponent,
+    GradesComponent,
+    GradeComponent,
+    EditGradeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
